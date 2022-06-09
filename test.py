7ee7c2e92abfb627da896f54bbc13b4136bec6eb
neeracha <neeracha1842@gmail.com>
2022-06-09 15:31:33 +0700

    gg2

--- test.py
+++ test.py
@@ -1,18 +1,16 @@
 import requests
-import urllib.request
-import requests
-import urllib.parse
-import sys
-def send_msg(msg):
-    LINE_ACCESS_TOKEN="hF4n9ndtL9yw2ip40tPF7iPulbrkypL4KFxs0DSsd9NS"
-    url = "https://notify-api.line.me/api/notify"
-    file = {'imageFile':open('./upload.jpg','rb')}
-    data = ({
-        'message':msg
-    })
-    LINE_HEADERS = {"Authorization":"Bearer "+LINE_ACCESS_TOKEN}
-    session = requests.Session()
-    r=session.post(url, headers=LINE_HEADERS, files=file, data=data)
-    print(r.text)
-send_msg("hello my first msg with line notify api")
+
+url = "https://notify-api.line.me/api/notify"
+
+payload={}
+files=[
+  ('imageFile',('anya.jpg',open('/C:/Users/Administrator/Documents/GitHub/line/lineAPI/anya.jpg','rb'),'image/jpeg'))
+]
+headers = {
+  'Authorization': 'Bearer hF4n9ndtL9yw2ip40tPF7iPulbrkypL4KFxs0DSsd9N'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload, files=files)
+
+print(response.text)
 
